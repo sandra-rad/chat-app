61ea75696ec121b1810aeb80ba4147a674ad929c
Sandra <radsandra@yahoo.com>
2020-08-22 16:52:15 +0300

    add leafletjs to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { tileLayer, latLng } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat-app';
+
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png')
+    ],
+    zoom: 5,
+    center: latLng([46.7712, 23.6236 ])
+  };
 }
